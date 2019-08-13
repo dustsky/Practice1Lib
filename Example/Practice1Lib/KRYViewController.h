71e36e798ba34fefa2b2590797918e9f7665ac31
codermy <gsmxsky@gmail.com>
2019-08-13 16:08:27 +0800

    Initial commit

---
+++ Example/Practice1Lib/KRYViewController.h
@@ -0,0 +1,13 @@
+//
+//  KRYViewController.h
+//  Practice1Lib
+//
+//  Created by codermy on 08/13/2019.
+//  Copyright (c) 2019 codermy. All rights reserved.
+//
+
+@import UIKit;
+
+@interface KRYViewController : UIViewController
+
+@end
---
+++ Example/Practice1Lib/KRYViewController.m
@@ -0,0 +1,29 @@
+//
+//  KRYViewController.m
+//  Practice1Lib
+//
+//  Created by codermy on 08/13/2019.
+//  Copyright (c) 2019 codermy. All rights reserved.
+//
+
+#import "KRYViewController.h"
+
+@interface KRYViewController ()
+
+@end
+
+@implementation KRYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
